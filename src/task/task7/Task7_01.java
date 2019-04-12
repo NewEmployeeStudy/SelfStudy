af920c98acffb99dd82cd15e6a001f0698629537
hiroshi1118 <hiroshi19721118.goodman@gmail.com>
2019-04-13 01:13:09 +0900

    class位置変更

--- src/task/task7/Task7_01.java
+++ src/task/task7/Task7_01.java
@@ -3,13 +3,13 @@ package task.task7;
 public class Task7_01 {
 
 
-	class Dog {
-		private String Name;
-		public void SetName( String nm ) {
-			Name = nm;
-		}
-		public void ShowProfile() {
-			System.out.println( "名前は、" + Name + "です。" );
-		}
+}
+class Dog {
+	private String Name;
+	public void SetName( String nm ) {
+		Name = nm;
+	}
+	public void ShowProfile() {
+		System.out.println( "名前は、" + Name + "です。" );
 	}
 }
--- src/task/task8/Task8_01.java
+++ src/task/task8/Task8_01.java
@@ -3,12 +3,12 @@ package task.task8;
 public class Task8_01 {
 
 
-	class Animal {
-		public String Name;
-		public int Age;
+}
+class Animal {
+	public String Name;
+	public int Age;
 
-		public void ShowProfile() {
-			System.out.println( "名前は、" + Name + "、" + Age + "歳です。" );
-		}
+	public void ShowProfile() {
+		System.out.println( "名前は、" + Name + "、" + Age + "歳です。" );
 	}
 }
