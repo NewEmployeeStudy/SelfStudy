2bbbfa675bd469206db6d1b4042995255543e457
hiroshi1118 <hiroshi19721118.goodman@gmail.com>
2019-04-13 01:11:43 +0900

    課題６−４

--- src/taskAns/task6/Task6_04.java
+++ src/taskAns/task6/Task6_04.java
@@ -1,5 +1,24 @@
 package taskAns.task6;
 
+import java.io.IOException;
+
 public class Task6_04 {
 
+	public static void main( String[] args ) throws IOException {
+
+		DrawTriangle( 3 );
+		System.out.println();
+		DrawTriangle( 4 );
+		System.out.println();
+		DrawTriangle( 5 );
+	}
+
+	static void DrawTriangle( int size ) {
+		for( int i = 0 ; i < size ; i++ ) {
+			for( int j = 0 ; j <= i ; j++ )
+				System.out.print( "$" );
+
+			System.out.println();
+		}
+	}
 }
